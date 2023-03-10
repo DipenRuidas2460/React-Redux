fc9fc81a6b75b69eb8d760874c49e83fc746dec2
DipenRuidas2460 <dipenruidas1996@gmail.com>
2023-03-10 16:02:29 +0530

    redux-complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Shop from "./components/Shop";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div className="container">
+        <Shop/>
+      </div>
+    </>
+  );
+}
+
+export default App;
